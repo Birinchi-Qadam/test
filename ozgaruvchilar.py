c94cfe28152895665e1aea854225410e04c3ec0d
Birinchi Qadam <habarlaralmashish@gmail.com>
2023-02-09 11:55:21 +0900

    uchinchi

---
+++ ozgaruvchilar.py
@@ -0,0 +1,26 @@
+# Bu yerdagi malumotlar python.sariq.dev saytidan olindi
+
+"""
+O'zgaruvchi —kompyuter xotirasida ma'lum bir qiymatni saqlash uchun ajratilgan joy. 
+Soddaroq qilib tushuntirsak, o'zgaruvchini quti, quti ichidagi narsani esa qiymat deb tasavvur qilish mumkin.
+Pythonda qiymatlar son, matn, ro'yxat va hokazo ko'rinishida bo'lishi mumkin.
+"""
+
+ism = "Falonchi"
+familya = "Pistonchiev"
+
+print(ism, familya)
+
+#  O'zgaruvchi (variable) bunday deyilishiga sabab, uning qiymati istalgan vaqt o'zgartirilishi mumkin
+
+ism = "Ali"
+familya = "Soliev"
+
+print(ism, familya)
+
+#  ifodalar
+
+son_1 = 10
+son_2 = 10
+
+print(son_1 + son_2)
