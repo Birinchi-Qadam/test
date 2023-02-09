02f14a1b5fe9757bab1e8933976fd2f5a77b73e4
Birinchi Qadam <habarlaralmashish@gmail.com>
2023-02-09 11:39:20 +0900

    ikkinchi

---
+++ main.py
@@ -0,0 +1,48 @@
+# print funksiyasi. -->  print() funktsiyasi matn yoki ifodalarni konsolga chiqarish vazifasini bajaradi.
+
+# 1. Matn bilan ishlashda matnni qo'sh tirnoq yoki bir tirnoq ichida yozish talab qilinadi.
+
+print("Hello World!")
+print('Assalomu alekum!')
+print('Salom men "Dasturlashni boshladim"')
+
+print("""Albatta bugun havo
+Kechagidan yaxshi""")  # """ """ uch tirnoqni matnni bir necha qatorga bo'lib yozish uchun ishlatish mumkin
+
+# qatorga bo'lishni yana bir yo'li
+
+print("Albatta bugun havo\n Kechagidan yaxshi") # \n belgisi qatorga bo'lishni ifodalaydi
+
+# Agar bir tirnoq ichida o'rik so'zini ishlatsak xato beradi shuning uchun \ belgisidan foydalaniladi
+
+print('Men bozordan o\'rik sotib oldim') # \ belgisi har qanday mahsus belgi oldidan qo'yiladi
+
+# Agar tirnoqlarsiz matn kiritsak hato berdi
+
+# print(Assalomu alekum)
+
+
+"""
+Arfimetik amallar
+"""
+
+print(2 + 2) # qo'shish amali
+print(2 - 2) # ayrish amali
+print(2 * 2) # ko'paytrish amali
+print(2 / 2) # bo'lish amali
+print(2 // 2) # Bo'lish va butun qismini olish
+print(2 ** 2) # daraja/ildiz
+print(2 % 2) # bo'linmani qoldig'ini olish
+
+
+"""
+print yordamida matn va ifodalarni jamlab chiqarish
+"""
+
+print("Qo'shish", 2 + 2)
+print("Ayirish", 2 - 2)
+print("Ko'paytirish", 2 * 2)
+print("Bo'lish", 2 / 2)
+print("Bo'lish va butun qismini olish", 2 // 2)
+print("Darajaga oshirish", 2 ** 2)
+print("Bo'linmani qoldig'ini olish", 2 % 2)
